7e38fb23b451b80b3f1c0934a3289bc9020c88c9
fdorval <francois.dorval@gmail.com>
2023-02-16 21:05:29 +0100

    commit

---
+++ src/main/java/com/example/demoB4/DemoB4Application.java
@@ -0,0 +1,13 @@
+package com.example.demoB4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoB4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoB4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoB4/DemoB4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoB4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoB4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
